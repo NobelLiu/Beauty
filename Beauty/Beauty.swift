f9afc5af8202ecdc3bd376e6d87ffb1e0f5b822d
Nobel <livi4tee@gmail.com>
2016-11-15 18:23:39 +0800

    Make Beauty File

---
+++ Beauty/Beauty.swift
@@ -0,0 +1,14 @@
+//
+//  Beauty.swift
+//  Beauty
+//
+//  Created by Nobel on 2016/11/15.
+//  Copyright © 2016年 Nobel. All rights reserved.
+//
+
+import UIKit
+
+class Beauty: NSObject {
+    var shared = [String:String]()
+    
+}
